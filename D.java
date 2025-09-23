2283071d4ec76aa391178a583cfe9aeb47cf0ce1
Lana da Silva Miranda <lanamiranda112@gmail.com>
2025-09-23 19:52:33 -0300

    Adição de MD4() em D.java
    Alteração pelo github

--- D.java
+++ D.java
@@ -3,8 +3,6 @@ public class D {
     private int d1;
     private String d2;
 
-
-    // 🔹 Métodos
     public void MD1() {
         System.out.println("Executando o método MD1()");
     }
@@ -12,4 +10,9 @@ public class D {
     public void MD2(int dado) {
         System.out.println("O dado recebido é: " + dado);
     }
+
+    public void MD4(){
+        System.out.println("Md4()";
+    }
+
 }
