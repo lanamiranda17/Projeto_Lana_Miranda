3cc5967e066e07d4d84329704e9f2adfc1a304eb
Lana da Silva Miranda <lanamiranda112@gmail.com>
2025-09-23 19:47:44 -0300

    Desfazendo erro no D.java

--- D.java
+++ D.java
@@ -12,8 +12,4 @@ public class D {
     public void MD2(int dado) {
         System.out.println("O dado recebido é: " + dado);
     }
-
-    public void MD4() {
-        System.out.println("MD4()");
-    }
 }
