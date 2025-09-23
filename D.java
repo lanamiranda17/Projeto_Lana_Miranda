540052ba439359d89bcca3f157cdcb47e023708f
Lana Miranda <lana.miranda@estudante.ufla.br>
2025-09-23 13:01:49 -0300

    Classe D adicionada

---
+++ D.java
@@ -0,0 +1,15 @@
+public class D {
+
+    private int d1;
+    private String d2;
+
+
+    // 🔹 Métodos
+    public void MD1() {
+        System.out.println("Executando o método MD1()");
+    }
+
+    public void MD2(int dado) {
+        System.out.println("O dado recebido é: " + dado);
+    }
+}
